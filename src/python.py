d2220ec46011feaa07a604bba2bae346e4737221
ElectricSlacks <35742210+ElectricSlacks@users.noreply.github.com>
2018-04-18 15:58:44 +0200

    Update python.py

--- src/python.py
+++ src/python.py
@@ -35,7 +35,7 @@ previous_block = blockchain[0]
 
 # How many blocks should we add to the chain
 # after the genesis block
-num_of_blocks_to_add = 20
+num_of_blocks_to_add = 300000000
 
 # Add blocks to the chain
 for i in range(0, num_of_blocks_to_add):
